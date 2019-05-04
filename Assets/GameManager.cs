7955529d7c6063d2380cd6d031fe24949a2f1bd6
ADigitalMindNet <50277107+ADigitalMindNet@users.noreply.github.com>
2019-05-04 22:14:51 +0100

    Initial Upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject theCube;
+	public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray,out hit,100f,groundLayer))
+		{
+			theCube.transform.position = hit.point;
+		}
+    }
+}
